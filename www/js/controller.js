cef2f4211e21ff5206a5131367201e40f63cf936
ekim1394 <simplyeugene94@gmail.com>
2017-09-04 20:47:18 -0400

    initializing project

---
+++ www/js/controller.js
@@ -0,0 +1,27 @@
+var Controller = function() {
+		var controller = {
+			self: null,
+			initialize: function() {
+				self = this;
+				console.log('Initializing controller');
+				self.bindEvents();
+				console.log("Binding events");
+				self.renderSearchView();
+				console.log('Rendered home page.');
+			},
+
+			bindEvents: function() {
+				// Add button click here
+			},
+
+			renderSearchView: function() {
+				// Render first view
+				var $projectTemplate = null;
+				$("#content").load("./views/search-project-view.html", function(data) {
+						$projectTemplate = $('.task').remove();
+					}
+				}
+			}
+			controller.initialize();
+			return controller;
+		}
